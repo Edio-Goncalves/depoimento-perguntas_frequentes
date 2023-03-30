3133fe6349c8c257ff0826099b38294646bc5788
Edio Goncalves <ediogoncalvesjr@hotmail.com>
2023-03-29 21:36:41 -0300

    Create the object for the inputs

---
+++ js/customer-testimonials.js
@@ -0,0 +1,30 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Facebook",
+    job: "Midia social",
+    img: "https://cdn-icons-png.flaticon.com/512/145/145802.png",
+    text: "Facebook é uma rede social que conecta usuários em todo o mundo. Por meio de perfis pessoais e profissionais, é possível encontrar e conhecer pessoas, acompanhar personalidades públicas e marcas, criar, consumir e compartilhar conteúdos e muito mais.",
+  },
+  {
+    id: 2,
+    name: "Whatsapp",
+    job: "App mensagem",
+    img: "https://cdn-icons-png.flaticon.com/512/174/174879.png",
+    text: "WhatsApp é um aplicativo multiplataforma de mensagens instantâneas e chamadas de voz para smartphones. Além de mensagens de texto, os usuários podem enviar imagens, vídeos e documentos em PDF, além de fazer ligações grátis por meio de uma conexão com a internet.",
+  },
+  {
+    id: 3,
+    name: "Linkedin",
+    job: "Midia social",
+    img: "https://cdn-icons-png.flaticon.com/512/174/174857.png",
+    text: "LinkedIn é uma plataforma de mídia social focada em negócios e emprego que funciona através de sites e aplicativos móveis. fundada em dezembro de 2002 e lançada em 5 de maio de 2003, de propriedade da Microsoft.",
+  },
+  {
+    id: 4,
+    name: "Instagram",
+    job: "Midia social",
+    img: "https://icon-library.com/images/instragram-icon/instragram-icon-16.jpg",
+    text: "O Instagram é uma rede social online de compartilhamento de fotos e vídeos entre seus usuários, que permite aplicar filtros digitais e compartilhá-los em uma variedade de serviços de redes sociais, como Facebook, Twitter, Tumblr e Flickr. ",
+  },
+];

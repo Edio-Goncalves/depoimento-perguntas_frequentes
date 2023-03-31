a72dd2b865457b230feb2b976afeed8e57bd414b
Edio Goncalves <ediogoncalvesjr@hotmail.com>
2023-03-30 23:12:22 -0300

    Fix bug in general question

--- js/general-question.js
+++ js/general-question.js
@@ -2,10 +2,14 @@ const questions = document.querySelectorAll(".question");
 
 questions.forEach(function (question) {
   const btn = question.querySelector(".question-btn");
+  console.log(question);
   btn.addEventListener("click", function () {
     questions.forEach((iten) => {
-      iten.classList.remove("show-text");
+      if (iten !== question) {
+        iten.classList.remove("show-text");
+      }
     });
     question.classList.toggle("show-text");
+    console.log(question);
   });
 });
